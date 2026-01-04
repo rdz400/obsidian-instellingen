7c68434d2a09a38ab4ea4583ee9aca0bcf0d8464
Ronald <48650980+rdz400@users.noreply.github.com>
2026-01-04 19:25:54 +0100

    Feature/add base sync (#1)
    * Voeg sync code voor bases toe
    
    * Update obsidian configuratie snippet
    
    * Update syncing code

--- sync-files.sh
+++ sync-files.sh
@@ -5,7 +5,12 @@ set -u
 source .env
 set +a
 
-echo $OBSIDIAN_VAULT_CONFIG
 
+echo "Syncing: $OBSIDIAN_VAULT_CONFIG"
 rsync -a --delete "$OBSIDIAN_VAULT_CONFIG"/ "obsidian-config/"
+
+echo "Syncing: $OBSIDIAN_VAULT_TEMPLATES"
 rsync -a --delete "$OBSIDIAN_VAULT_TEMPLATES"/ "obsidian-templates/"
+
+echo "Syncing: $OBSIDIAN_VAULT_BASES"
+rsync -a --delete "$OBSIDIAN_VAULT_BASES"/ "obsidian-bases/"
